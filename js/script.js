a592ba1909c0f75bb38b0d2386c3c151217da6e4
Guzel <viproo@bk.ru>
2024-10-25 18:28:28 +0300

    refactoring our-work section

--- js/script.js
+++ js/script.js
@@ -51,49 +51,12 @@ var mySwiper = new Swiper(".our-works__swiper", {
   spaceBetween: 15,
   effect: "fade",
   pagination: {
-    el: ".swiper-pagination",
+    el: ".swiper-pagination-2",
     clickable: true,
   },
   navigation: {
-    nextEl: ".our-works .arrow_next",
-    prevEl: ".our-works .arrow_prev",
+    nextEl: ".our-works .arrow-2_next",
+    prevEl: ".our-works .arrow-2_prev",
   },
 });
 
-// Обработчик события slideChange
-mySwiper.on('slideChange', function () {
-var currentSlideId = this.slides[this.activeIndex].getAttribute('data-id');
-var allWorksteps = document.querySelectorAll(
-  ".our-works__coll[data-our-works]"
-);
-
-allWorksteps.forEach(function(workstep) {
-  var workstepValue = workstep.getAttribute("data-our-works");
-  
-  if (workstepValue === currentSlideId) {
-  workstep.classList.add('active');
-  } else {
-  workstep.classList.remove('active');
-  }
-});
-});
-
-// Добавляем обработчик события клика для элементов с атрибутом data-worksteps
-document
-  .querySelectorAll(".our-works__coll[data-our-works]")
-  .forEach(function (element) {
-    element.addEventListener("click", function () {
-      // Получаем значение атрибута data-our-works текущего элемента
-      var targetSlideId = this.getAttribute("data-our-works");
-
-      // Ищем индекс слайда с атрибутом data-id, соответствующего targetSlideId
-      var targetSlideIndex = Array.from(mySwiper.slides).findIndex(function (
-        slide
-      ) {
-        return slide.getAttribute("data-id") === targetSlideId;
-      });
-
-      // Переключаем Swiper на целевой слайд
-      mySwiper.slideTo(targetSlideIndex);
-    });
-  });
